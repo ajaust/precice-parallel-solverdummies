9daeeb41c1fa36029b324aa51da4e5db322fa407
Alexander Jaust <alexander.jaust@ipvs.uni-stuttgart.de>
2021-05-04 08:28:11 +0200

    update README

---
+++ README.md
@@ -0,0 +1,86 @@
+# Parallel solver dummy README
+
+This repository contains contains two solver dummies for the coupling library [preCICE](https://github.com/precice/precice) that can be executed in parallel. The solver dummies can also be mixed with each other, i.e., each solver can play any role in the coupling!
+
+## Requirements
+
+C++ dummy:
+- CMake
+- A C++ compiler
+- MPI (only tested with OpenMPI)
+- preCICE v2 (only tested with v2.2.0)
+
+The Python dummy additionally needs:
+- Python 3
+- mpi4py
+- [Python bindings for preCICE](https://github.com/precice/python-bindings)
+
+If you have preCICE and the Python bindings installed, you should have all requirements installed.
+
+## Compilation
+
+Compile the C++ solver dummy and copy it back to the root of the repository:
+
+```
+mkdir build
+cd build
+cmake ..
+make
+cp solverdummy-cpp-parallel ..
+cd ..
+```
+
+## Test solvers
+
+`N` and `M` refer to the number of ranks one want to use. Start with `1` in order to run the serial computation and increase `N` and `M` afterwards to have truly parallel computations.
+
+Further information:
+- `N` and `M` may be different numbers.
+- The solver have to run in different shells or you have to send the first solver to the background.
+- You pass the participant's name and the corresponding mesh name on the terminal. The name and mesh have to be one of the following
+    - `SolverOne` and `MeshOne`
+    - `SolverTwo` and `MeshTwo`
+- Replace `mpirun` by the suitable MPI wrapper of your machine.
+
+### Setup 1: C++ and C++
+
+```
+mpirun -n N ./solverdummy-cpp-parallel precice-config-parallel.xml SolverOne MeshOne
+mpirun -n M ./solverdummy-cpp-parallel ../precice-config-parallel.xml SolverTwo MeshTwo
+```
+
+Example:
+```
+mpirun -n 4 ./solverdummy-cpp-parallel precice-config-parallel.xml SolverOne MeshOne
+mpirun -n 2 ./solverdummy-cpp-parallel precice-config-parallel.xml SolverTwo MeshTwo
+```
+
+### Setup 2: Python and Python
+
+```
+mpirun -n N python3 solverdummy-python-parallel.py precice-config-parallel.xml SolverOne MeshOne
+mpirun -n M python3 solverdummy-python-parallel.py precice-config-parallel.xml SolverTwo MeshTwo
+```
+
+Example:
+```
+mpirun -n 4 python3 solverdummy-python-parallel.py precice-config-parallel.xml SolverOne MeshOne
+mpirun -n 2 python3 solverdummy-python-parallel.py precice-config-parallel.xml SolverTwo MeshTwo
+```
+
+### Setup 3: C++ and Python
+
+```
+mpirun -n N ./solverdummy-cpp-parallel precice-config-parallel.xml SolverOne MeshOne
+mpirun -n M python3 solverdummy-python-parallel.py precice-config-parallel.xml SolverTwo MeshTwo
+```
+
+Example:
+```
+mpirun -n 4 ./solverdummy-cpp-parallel precice-config-parallel.xml SolverOne MeshOne
+mpirun -n 2 python3 solverdummy-python-parallel.py precice-config-parallel.xml SolverTwo MeshTwo
+```
+
+## Remarks
+
+- The codes are heavily based on the solver dummies of [preCICE](https://github.com/precice/precice) and the corresponding [Python bindings](https://github.com/precice/python-bindings).
---
+++ solverdummy-cpp-parallel.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <sstream>
+#include <mpi.h>
+#include "precice/SolverInterface.hpp"
+
+int main(int argc, char **argv)
+{
+  MPI_Init( nullptr, nullptr );
+
+
+  int commRank = -1;
+  MPI_Comm_rank(MPI_COMM_WORLD, &commRank);
+
+  int commSize = -1;
+  MPI_Comm_size(MPI_COMM_WORLD, &commSize);
+
+  using namespace precice;
+  using namespace precice::constants;
+
+  if (argc != 4) {
+    std::cout << "Usage: ./solverdummy configFile solverName meshName\n\n";
+    std::cout << "Parameter description\n";
+    std::cout << "  configurationFile: Path and filename of preCICE configuration\n";
+    std::cout << "  solverName:        SolverDummy participant name in preCICE configuration\n";
+    std::cout << "  meshName:          Mesh in preCICE configuration that carries read and write data\n";
+    return 1;
+  }
+
+  const std::string configFileName(argv[1]);
+  const std::string solverName(argv[2]);
+  const std::string meshName(argv[3]);
+
+  std::cout  << "DUMMY (" << commRank << "): Running solver dummy with preCICE config file \"" << configFileName << "\", participant name \"" << solverName << "\", and mesh name \"" << meshName << "\".\n";
+
+  SolverInterface interface(solverName, configFileName, commRank, commSize);
+
+  int         meshID     = interface.getMeshID(meshName);
+  int         dimensions = interface.getDimensions();
+  std::string dataWriteName;
+  std::string dataReadName;
+  int         numberOfVertices = 3;
+
+  if (solverName == "SolverOne") {
+    dataWriteName = "dataOne";
+    dataReadName  = "dataTwo";
+  }
+  if (solverName == "SolverTwo") {
+    dataReadName  = "dataOne";
+    dataWriteName = "dataTwo";
+  }
+  const int readDataID  = interface.getDataID(dataReadName, meshID);
+  const int writeDataID = interface.getDataID(dataWriteName, meshID);
+
+  std::vector<double> readData(numberOfVertices * dimensions);
+  std::vector<double> writeData(numberOfVertices * dimensions);
+  std::vector<double> vertices(numberOfVertices * dimensions);
+  std::vector<int>    vertexIDs(numberOfVertices);
+
+  for (int i = 0; i < numberOfVertices; i++) {
+    for (int j = 0; j < dimensions; j++) {
+      vertices.at(j + dimensions * i)  = i;
+      readData.at(j + dimensions * i)  = i;
+      writeData.at(j + dimensions * i) = i;
+    }
+  }
+
+  interface.setMeshVertices(meshID, numberOfVertices, vertices.data(), vertexIDs.data());
+
+  double dt = interface.initialize();
+
+  while (interface.isCouplingOngoing()) {
+
+    if (interface.isActionRequired(actionWriteIterationCheckpoint())) {
+      std::cout << "DUMMY (" << commRank << "): Writing iteration checkpoint\n";
+      interface.markActionFulfilled(actionWriteIterationCheckpoint());
+    }
+
+    if (interface.isReadDataAvailable()) {
+      interface.readBlockVectorData(readDataID, numberOfVertices, vertexIDs.data(), readData.data());
+    }
+
+    for (int i = 0; i < numberOfVertices * dimensions; i++) {
+      writeData.at(i) = readData.at(i) + 1;
+    }
+
+    if (interface.isWriteDataRequired(dt)) {
+      interface.writeBlockVectorData(writeDataID, numberOfVertices, vertexIDs.data(), writeData.data());
+    }
+
+    dt = interface.advance(dt);
+
+    if (interface.isActionRequired(actionReadIterationCheckpoint())) {
+      std::cout  << "DUMMY (" << commRank << "): Reading iteration checkpoint\n";
+      interface.markActionFulfilled(actionReadIterationCheckpoint());
+    } else {
+      std::cout  << "DUMMY (" << commRank << "): Advancing in time\n";
+    }
+  }
+
+  interface.finalize();
+  std::cout  << "DUMMY (" << commRank << "): Closing C++ solver dummy...\n";
+
+  MPI_Finalize();
+
+  return 0;
+}
